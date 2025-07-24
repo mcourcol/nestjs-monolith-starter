8f4fcc2e740bf547688659aec2409b2dcfcfd17d
Maxime Courcol <courcol.maxime@gmail.com>
2025-07-24 18:16:27 +0200

    Add basic health check with Terminus

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { LoggerModule } from './core/logger/logger.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule],
+  imports: [ConfigModule, LoggerModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
